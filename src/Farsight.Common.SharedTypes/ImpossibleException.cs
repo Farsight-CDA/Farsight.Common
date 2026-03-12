41273ab2cf44bfd893d8496506f908f0d267c1de
PoolPirate <eliaswolf1609@gmail.com>
2026-03-12 11:56:29 +0100

    feat: add ImpossibleException

---
+++ src/Farsight.Common.SharedTypes/ImpossibleException.cs
@@ -0,0 +1,8 @@
+﻿namespace Farsight.Common;
+
+/// <summary>
+/// Represents an exception that is thrown in code paths that are supposed to be unreachable.
+/// </summary>
+public sealed class ImpossibleException() : Exception("Unreachable code path called")
+{
+}
